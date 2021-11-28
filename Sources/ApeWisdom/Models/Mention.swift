1920d9a4500b25d4f3cff09edd6c1ed85f85990a
antl1p <anthony@lipscomb.dev>
2021-11-27 22:14:25 -0700

    bring up to date with SwiftUI

--- Sources/ApeWisdom/Models/Mention.swift
+++ Sources/ApeWisdom/Models/Mention.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct Mention: Codable {
+public struct Mention {
     public let rank: Int
     public let ticker: String
     public let name: String
@@ -8,8 +8,10 @@ public struct Mention: Codable {
     public let upvotes: String?
     public let last24HourRank: String?
     public let last24HourMentions: String?
-    
-        enum CodingKeys: String, CodingKey {
+}
+
+extension Mention: Codable {
+    enum CodingKeys: String, CodingKey {
         case rank
         case ticker
         case name
@@ -18,4 +20,8 @@ public struct Mention: Codable {
         case last24HourRank = "rank_24h_ago"
         case last24HourMentions = "mentions_24h_ago"
     }
+}
+
+extension Mention: Identifiable {
+   public var id: String { ticker }
 }
