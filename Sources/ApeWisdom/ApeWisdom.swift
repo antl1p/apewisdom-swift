a08da1626917d216d6e16f140940ac4669fddb6a
antl1p <anthony@lipscomb.dev>
2021-11-27 22:07:09 -0700

    make the filter method public

--- Sources/ApeWisdom/ApeWisdom.swift
+++ Sources/ApeWisdom/ApeWisdom.swift
@@ -4,7 +4,7 @@ public struct ApeWisdom {
     var baseUrl: String = "https://apewisdom.io/api/v1.0"
     
     @available(macOS 12, iOS 13, *)
-    func filter(_ filter: Filter, page: Int? = nil) async throws -> MentionResponse {
+    public func filter(_ filter: Filter, page: Int? = nil) async throws -> MentionResponse {
         try await FilterRepository.shared.list(filter: filter, page: page)
     }
 }
