922b0f9fbfb0f3d3d596faa034475156fb74367a
antl1p <anthony@lipscomb.dev>
2021-11-27 21:49:22 -0700

    fix package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .iOS(.v15),
         .macOS(.v12),
         .tvOS(.v15),
-        .watchOS(.v7.6)
+        .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
